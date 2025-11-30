a261f174d8b9db883c35d021f5a94e9658015522
Harsh Katiyar <142113138+dev-harshkatiyar@users.noreply.github.com>
2025-11-30 11:24:02 +0530

    Time: 285 ms (0.00%) | Memory: 193.2 MB (0.00%) - LeetSync

---
+++ 4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countElements(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> list=new ArrayList<>(map.keySet());
+        Collections.sort(list,Collections.reverseOrder());
+
+        int count=0, greater=0;
+        for(int val : list){
+            if(greater>=k) count+=map.get(val);
+            greater+=map.get(val);
+        }
+        return count;
+    }
+}
