e872c2c825dc51d917b4be8f55b30dc300695fbd
Harsh Katiyar <142113138+dev-harshkatiyar@users.noreply.github.com>
2025-11-30 13:37:55 +0530

    Time: 1 ms (55.03%) | Memory: 43.6 MB (23.11%) - LeetSync

--- 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.java
@@ -1,7 +1,6 @@
 class Solution {
     public int countElements(int[] nums) {
         int count=0;
-        int total=0;
         int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
         for(int i=0;i<nums.length;i++){
