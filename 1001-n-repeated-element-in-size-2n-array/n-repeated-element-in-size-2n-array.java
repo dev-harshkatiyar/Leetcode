4e263e477f442cc94bc0bf5bddfe37e729350e2a
Harsh Katiyar <142113138+dev-harshkatiyar@users.noreply.github.com>
2026-01-02 17:14:32 +0530

    Time: 17 ms (29.06%) | Memory: 47.7 MB (25.57%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -3,10 +3,12 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int num:map.keySet()){
             if(map.get(num)>1){
                 return num;
             }
         }
-        return -1;
+        return 0;
     }
 }
