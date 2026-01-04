8c76d54a02e287c721fa6f99781a2c38a1954999
Harsh Katiyar <142113138+dev-harshkatiyar@users.noreply.github.com>
2026-01-04 23:43:56 +0530

    Time: 508 ms (59.42%) | Memory: 0B (100.00%) - LeetSync

--- 3996-find-books-with-polarized-opinions/find-books-with-polarized-opinions.sql
+++ 3996-find-books-with-polarized-opinions/find-books-with-polarized-opinions.sql
@@ -1,13 +1,14 @@
-select book_id, title ,author,genre ,pages,
-max(session_rating)-min(session_rating) rating_spread ,
-round(sum(case when session_rating>3 or session_rating <3 then 1 else 0 end )*1.0/count(*),2) polarization_score 
-from books
-join reading_sessions 
-using(book_id)
-group by 1,2,3,4,5
-having count(*)>4
-and sum(case when session_rating>3 then 1 else 0 end)>0
-and sum(case when session_rating<3 then 1 else 0 end)>0
-and 
-polarization_score>=0.6
+# Write your MySQL query statement below
+select b.book_id,b.title,b.author,b.genre,b.pages,max(rs.session_rating)-min(rs.session_rating) as rating_spread,
+round(sum(case 
+when rs.session_rating>=4 or rs.session_rating<=2 then 1 else 0 
+end)*1.0/count(rs.session_rating),2) as polarization_score
+from books as b
+left join reading_sessions as rs
+on b.book_id=rs.book_id
+group by b.book_id,b.title,b.author,b.genre,b.pages
+having count(rs.session_rating)>=5
+and sum(case when rs.session_rating>=4 then 1 else 0 end)>0
+and sum(case when rs.session_rating<=2 then 1 else 0 end)>0
+and polarization_score>=0.6
 order by polarization_score desc,title desc
