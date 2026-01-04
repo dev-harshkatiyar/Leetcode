40d2090df79d437231aff5d92f14055e43a5eff8
Harsh Katiyar <142113138+dev-harshkatiyar@users.noreply.github.com>
2026-01-05 00:05:13 +0530

    Time: 508 ms (59.42%) | Memory: 0B (100.00%) - LeetSync

--- 3996-find-books-with-polarized-opinions/find-books-with-polarized-opinions.sql
+++ 3996-find-books-with-polarized-opinions/find-books-with-polarized-opinions.sql
@@ -6,7 +6,7 @@ end)/count(rs.session_rating),2) as polarization_score
 from books as b
 left join reading_sessions as rs
 on b.book_id=rs.book_id
-group by b.book_id,b.title,b.author,b.genre,b.pages
+group by b.book_id
 having count(rs.session_rating)>=5
 and sum(case when rs.session_rating>=4 then 1 else 0 end)>0
 and sum(case when rs.session_rating<=2 then 1 else 0 end)>0
