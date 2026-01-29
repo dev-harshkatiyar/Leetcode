e16c387936ac952981e6c22339a7c8e974228f0b
Harsh Katiyar <142113138+dev-harshkatiyar@users.noreply.github.com>
2026-01-30 00:45:31 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (81.27%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -19,8 +19,8 @@ public class Solution {
             fast=fast.next.next;
 
             if(slow==fast){
-            return true;
-        }
+                return true;
+            }
         }
         return false;
     }
