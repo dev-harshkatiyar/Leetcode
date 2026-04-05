1edf87171b4ace36a8fc6144d61b480644336c78
Harsh Katiyar <142113138+dev-harshkatiyar@users.noreply.github.com>
2026-04-06 01:50:35 +0530

    Time: 6 ms (49.40%) | Memory: 45.5 MB (74.99%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int ydir=0,xdir=0;
+        for(int i=0;i<moves.length();i++){
+            char ch=moves.charAt(i);
+            if(ch=='U'){
+                ydir++;
+            }
+            else if(ch=='D'){
+                ydir--;
+            }
+            else if(ch=='L'){
+                xdir--;
+            }
+            else{
+                xdir++;
+            }
+        }
+        return ydir==0 && xdir==0;
+    }
+}
