6c2bace51d5decb687505b04dfe3f0fd5486748c
Harsh Katiyar <142113138+dev-harshkatiyar@users.noreply.github.com>
2026-07-05 03:08:58 +0530

    Time: 6 ms (72.14%) | Memory: 45 MB (43.85%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,40 +1,28 @@
 class Solution {
-    // Function to find number of ways to assign + or - to reach the target
-    public int Count_expressions(int[] nums, int target) {
-        int n = nums.length;
-
-        // First calculate total sum of all numbers
-        int totalSum = 0;
-        for (int num : nums) totalSum += num;
-
-        // If (target + totalSum) is odd or target > totalSum, no valid partition exists
-        if ((totalSum + target) % 2 != 0 || Math.abs(target) > totalSum) return 0;
-
-        // We now need to count subsets with sum = (target + totalSum) / 2
-        int newTarget = (totalSum + target) / 2;
-
-        // Create DP table: dp[i][j] = number of ways to make sum j using first i numbers
-        int[][] dp = new int[n + 1][newTarget + 1];
-
-        // Base case: One way to form sum 0 (by taking no elements)
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
+    public static int Count_expressions(int[] nums,int target){
+        int n=nums.length;
+        int totalsum=0;
+        for(int i=0;i<n;i++){
+            totalsum+=nums[i];
         }
+        int T=(target+totalsum)/2;
+        if((target+totalsum)%2!=0 || Math.abs(target)>totalsum) return 0;
+        int[][] dp=new int[n][T+1];
+        if(nums[0]==0) dp[0][0]=2;
+        else dp[0][0]=1;
+        if(nums[0]!=0 && nums[0]<=T) dp[0][nums[0]]=1;
 
-        // Fill DP table iteratively
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= newTarget; j++) {
-                // Exclude current element
-                dp[i][j] = dp[i - 1][j];
-
-                // Include current element if it does not exceed current target j
-                if (nums[i - 1] <= j) {
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=T;t++){
+                int not_take=0+dp[i-1][t];
+                int take=0;
+                if(nums[i]<=t){
+                    take=dp[i-1][t-nums[i]];
                 }
+                dp[i][t]=take+not_take;
             }
         }
-
-        return dp[n][newTarget];
+        return dp[n-1][T];
     }
     public int findTargetSumWays(int[] nums, int target) {
         return Count_expressions(nums,target);
